553a35a154c5508287c4188ab2408066fe6fe106
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2026-02-04 22:33:08 -0300

    Refactor MRF script for expanding window forecasts
    Updated the script to include expanding window forecasts and adjusted model parameters. Added functionality to save forecasts from both fixed and expanding window models.

--- 7.run_mrf.R
+++ 7.run_mrf.R
@@ -2,47 +2,78 @@ library(MacroRF)
 
 source("functions/functions.R")
 source("functions/fixed_window.R")
+source("functions/expanding_window.R")
+
 
 load("data/rawdata.RData")
 df <- dados
 
-# call_mrf ####
+# call_mrf fixed window ####
 
-model_name = "MRF0202"
-
-# FAARRF 12 lags ####
-n_y_lags = 12
-n_lags_of_each_var = 2 
-n_lags_of_5_factors = 12
-n_lags_j = 12
-n_maf_components = 2 
-lin.model = "FAARRF"
-
-
-# FAARRF 8 lags ####
-n_y_lags = 8
-n_lags_of_each_var = 2 
-n_lags_of_5_factors = 8
-n_lags_j = 8
-n_maf_components = 2 
-lin.model = "FAARRF"
-
+model_name = "MRF_exp_12lags"
 
 model_list = list()
 
 for (i in c(1:12)){
   cat("horizonte ", i,"\n")
-  model = fixed_window(df=df, lin.model = "FAARRF", horizon = i,n_y_lags = 12,
+  model = fixed_window(df=df, lin.model = "FAARRF", horizon = i,
+                       n_y_lags = 12,
                        n_lags_of_each_var = 2 ,
                        n_lags_of_5_factors = 12,
                        n_lags_j = 12,
-                       n_maf_components = 2)
+                       n_maf_components = 2,
+                       oos.pos = 312)
   model_list[[i]] = model
 }
 
 
+
+# salvando os forecasts de fixed window
+
 forecasts <-  Reduce(cbind,lapply(model_list, function(x)x$pred))
 
+
+
+
+
+
+# call_mrf expanding window ####
+
+for (i in 1:12){
+  cat("horizonte ", i,"\n")
+  model = expanding_window(df=df, lin.model = "FAARRF", horizon = i,
+                           n_y_lags = 12,
+                           n_lags_of_each_var = 2,
+                           n_lags_of_5_factors = 12,
+                           n_lags_j = 12,
+                           n_maf_components = 2,
+                           oos.pos = 312)
+  model_list[[i]] = model
+}
+
+
+# salvando os forecasts de expanding window
+
+forecast_list <- list()
+
+for ( i in 1:2){
+  
+  forecast_list[[i]] <- list()
+  
+  for ( j in 1:oos.pos/12){
+forecasts <-  Reduce(cbind,lapply(model_list, function(x)x[[i]][[j]]$pred))
+
+forecast_list[[i]][[j]] <- forecasts
+    }
+}
+
+model_list[[2]][[2]]$pred
+
+
+
+
+
+# finalizando as previsoes ####
 
 
 # o accumulate_model calcula as diagonais, sendo assim, os valores de previsao de 3 e 6 meses e adiciona as colunas referetnes
